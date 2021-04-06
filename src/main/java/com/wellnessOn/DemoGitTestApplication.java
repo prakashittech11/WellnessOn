8c779630b8ea52252ddbc6e7068b976711756971
prakash@Krishan <prakashittech11@gmail.com>
2021-04-06 11:44:31 +0530

    Test Commit

---
+++ src/main/java/com/wellnessOn/DemoGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.wellnessOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellnessOn/DemoGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellnessOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
